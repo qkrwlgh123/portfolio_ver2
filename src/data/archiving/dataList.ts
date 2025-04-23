6db9175bc1b1b879481e011d3e85b7256489af1c
qkrwlgh123 <jiho1024@naver.com>
2025-04-24 08:58:21 +0900

    팝업스토어 회원 유입률 기능 가입자 수 산정 기준 정정(2~3주 -> 1주)

--- src/data/archiving/dataList.ts
+++ src/data/archiving/dataList.ts
@@ -29,20 +29,8 @@ export const longDataList = [
     image: DocumentsImage,
     link: [
       {
-        link: "https://therapeutic-iridium-a60.notion.site/5d4cc7a33f7843f48c77072ae0b1f039?pvs=25",
-        text: "캘린더 기반 일정 관리 플랫폼 개발 중 작성했던 달성률 안내 팝업 기획안",
-      },
-      {
-        link: "https://therapeutic-iridium-a60.notion.site/React-query-API-d7458219729547ebadcdcc6190919535?pvs=25",
-        text: "캘린더 기반 일정 관리 플랫폼 개발 중 작성했던 Tanstack query 도입 및 리팩토링 계획안",
-      },
-      {
-        link: "https://therapeutic-iridium-a60.notion.site/2024-05-25-ca8565f82b924841b986df0621f82c2d?pvs=4",
-        text: "React 기반 프론트엔드 스터디를 진행하며 제작한 구현 과제 일부",
-      },
-      {
         link: "https://therapeutic-iridium-a60.notion.site/PPT-cdb764426ef84c51b2f8af1512a3c6fb?pvs=74",
-        text: "(주) 웅진씽크빅 재직 당시, 교육 컨텐츠 통합 웹 뷰어 및 중학 교육 플랫폼 업무 진행 발표 자료",
+        text: "(주) 웅진씽크빅 인턴 사원 재직 당시, 교육 컨텐츠 통합 웹 뷰어 및 중학 교육 플랫폼 업무 진행 발표 자료",
       },
       {
         link: "https://therapeutic-iridium-a60.notion.site/Zip-viewer-25af5a692c9c48a59e8f3ac5f988749a",
@@ -54,6 +42,6 @@ export const longDataList = [
       },
     ],
     subtitle:
-      "기업에서 다양한 프로젝트 및 기술발표, 개인 프로젝트를 진행하며 작성했던 자료 모음",
+      "기업에서 다양한 프로젝트 및 기술발표를 준비하며 작성했던 자료 모음",
   },
 ];
